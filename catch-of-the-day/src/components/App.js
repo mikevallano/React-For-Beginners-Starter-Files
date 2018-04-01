16ffa898c86a0699525a1599a00d144181c88b2f
mike vallano <mikevallano@gmail.com>
2018-04-01 10:00:24 -0500

    Add form to add fish and update state.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -4,6 +4,18 @@ import Order from './Order';
 import Inventory from './Inventory';
 
 class App extends React.Component {
+  state = {
+    fishes: {},
+    order: {}
+  }
+  addFish = fish => {
+    // 1. make copy of existing state
+    const fishes = {...this.state.fishes};
+    // 2. set new value(s)
+    fishes[`fish${Date.now()}`] = fish; //interpolate the current time to the fish key
+    // 3. Use React's setState to set new state
+    this.setState({fishes}); // in es6, if your key value have the same name, you can just use one. it's equal to fishes: fishes
+  }
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,7 +23,7 @@ class App extends React.Component {
           <Header tagline="so so fresh" years_in_biz={100}/>
         </div>
         {<Order />}
-        {<Inventory />}
+        {<Inventory addFish={this.addFish}/>}
       </div>
     )
   }
--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 class Order extends React.Component {
+  logClick = () => {
+    console.log('got clicked')
+  }
   render() {
     return (
-      <div className="order">
+      <div className="order" onClick={this.logClick}>
         Order Component
       </div>
     )
